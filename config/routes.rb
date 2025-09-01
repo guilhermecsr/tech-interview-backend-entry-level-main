65a41f1459f7daa2541f6e735298da6a7c9998f5
Guilherme <guicsramos@gmail.com>
2025-08-31 23:07:23 -0300

    add cart routes and specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+
   resources :products
+  resource :cart, only: [:show, :create]
+
+  delete 'cart/:product_id', to: 'carts#destroy', as: :remove_from_cart
+
+  post 'cart/add_item', to: 'carts#add_item'
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
